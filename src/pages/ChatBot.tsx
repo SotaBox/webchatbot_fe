bac5c88bb67003f22248bd748e5293061f0cc706
Thang Ngo <ngosythang123456@gmail.com>
2024-06-14 10:56:03 +0700

    refactor:chatbot-ui

--- src/pages/ChatBot.tsx
+++ src/pages/ChatBot.tsx
@@ -133,6 +133,75 @@ I don't have an age like humans do because I'm an artificial intelligence create
             <div className="flex flex-col space-y-4 h-[500px] overflow-auto">
               {isNewListMessage ? (
                 <div className="flex flex-col space-y-6 max-w-6xl">
+                  {question?.map((q) => {
+                    return (
+                      <>
+                        {q.role === UserRole.USER ? (
+                          <div className="flex flex-row justify-end space-x-4">
+                            <div className="flex flex-col space-y-1 rounded-lg shadow-md p-2">
+                              <h3 className="text-left text-black font-medium">
+                                You
+                              </h3>
+                              <p className="text-black max-w-sm md:max-w-xl">
+                                {q.content}
+                              </p>
+                            </div>
+                            <Avatar
+                              icon={<AvatarIcon />}
+                              className="shadow-md flex"
+                              classNames={{
+                                base: "bg-gradient-to-br from-[#71717A] to-[#27272A]",
+                                icon: "text-white/80",
+                              }}
+                            />
+                          </div>
+                        ) : (
+                          <div className="flex flex-row space-x-4">
+                            <Avatar
+                              icon={
+                                <Avatar
+                                  icon={
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      fill="none"
+                                      viewBox="0 0 24 24"
+                                      strokeWidth={1.5}
+                                      stroke="currentColor"
+                                      className="size-6"
+                                    >
+                                      <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z"
+                                      />
+                                    </svg>
+                                  }
+                                  className="shadow-md"
+                                  classNames={{
+                                    base: "bg-gradient-to-br from-[#0369a1] to-[#082f49]",
+                                    icon: "text-white/80",
+                                  }}
+                                />
+                              }
+                              className="shadow-md flex"
+                              classNames={{
+                                base: "bg-gradient-to-br from-[#71717A] to-[#27272A]",
+                                icon: "text-white/80",
+                              }}
+                            />
+                            <div className="flex flex-col space-y-1 rounded-lg shadow-md p-2 bg-sky-800">
+                              <h3 className="text-left text-white font-medium">
+                                ChatBot
+                              </h3>
+                              <p className="text-white max-w-sm md:max-w-xl">
+                                {q.content}
+                              </p>
+                            </div>
+                          </div>
+                        )}
+                      </>
+                    );
+                  })}
                   <div ref={messagesEndRef} />
                 </div>
               ) : (
